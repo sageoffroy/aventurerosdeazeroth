be4c8447bcb63c69859c03bd9a194518345e8ca2
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-18 14:13:15 -0300

    Declare normalized custom spell runtime

---
+++ modules/mod-spelldraft/src/CustomSpellScaling.h
@@ -0,0 +1,7 @@
+#ifndef AVENTUREROS_CUSTOM_SPELL_SCALING_H
+#define AVENTUREROS_CUSTOM_SPELL_SCALING_H
+
+void ConfigureCustomSpellScaling(bool enabled);
+void AddCustomSpellScalingScripts();
+
+#endif
