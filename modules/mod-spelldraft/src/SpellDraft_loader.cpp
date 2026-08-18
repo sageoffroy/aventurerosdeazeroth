781c822448afc29a7cdec97481ca7ee48579a8cd
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-18 14:14:32 -0300

    Register normalized custom spell scaling script

--- modules/mod-spelldraft/src/SpellDraft_loader.cpp
+++ modules/mod-spelldraft/src/SpellDraft_loader.cpp
@@ -1,8 +1,11 @@
+#include "CustomSpellScaling.h"
+
 void AddSpellDraftScripts();
 void AddAdventurerClassScripts();
 
 void Addmod_spelldraftScripts()
 {
     AddSpellDraftScripts();
+    AddCustomSpellScalingScripts();
     AddAdventurerClassScripts();
 }
