f1ac82338fd6b4507c2dfe8f21c31de035477783
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-18 22:54:19 -0300

    fix(spelldraft): load Adventurer combo compatibility

--- modules/mod-spelldraft/src/SpellDraft_loader.cpp
+++ modules/mod-spelldraft/src/SpellDraft_loader.cpp
@@ -2,10 +2,12 @@
 
 void AddSpellDraftScripts();
 void AddAdventurerClassScripts();
+void AddAdventurerComboPointsScripts();
 
 void Addmod_spelldraftScripts()
 {
     AddSpellDraftScripts();
     AddCustomSpellScalingScripts();
     AddAdventurerClassScripts();
+    AddAdventurerComboPointsScripts();
 }
