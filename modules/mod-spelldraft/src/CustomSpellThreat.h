01b321732fa251195d15df189647416dc4269b54
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-21 09:53:46 -0300

    Expose normalized threat rules to spell scripts

--- modules/mod-spelldraft/src/CustomSpellThreat.h
+++ modules/mod-spelldraft/src/CustomSpellThreat.h
@@ -1,6 +1,16 @@
 #ifndef AVENTUREROS_CUSTOM_SPELL_THREAT_H
 #define AVENTUREROS_CUSTOM_SPELL_THREAT_H
 
+#include "Define.h"
+
+class Player;
+
 void ConfigureCustomSpellThreat(bool enabled);
+bool GetCustomSpellThreatRule(
+    Player const* player,
+    uint32 spellId,
+    int32& flatMod,
+    float& pctMod,
+    float& apPctMod);
 
 #endif
